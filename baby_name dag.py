1338bb858a6d5eeb7be1cd1e9f6d7366c8e37c19
zfreeze <zdfreeze11@gmail.com>
2025-02-23 12:49:55 -0600

    correct file path for file download

---
+++ baby_name dag.py
@@ -0,0 +1,53 @@
+from datetime import datetime, timedelta
+import pendulum
+from airflow.decorators import dag, task
+from airflow.providers.postgres.hooks.postgres import PostgresHook
+from airflow.providers.http.hooks.http import HttpHook
+
+
+# Define default arguments
+default_args = {
+    'owner': 'zfreeze',
+    'retries': 2,
+    'retry_delay': timedelta(minutes=30)
+}
+
+# DAG definition using @dag decorator
+@dag(
+    start_date=datetime(2024, 5, 1, 14, 0, 0, tzinfo=pendulum.timezone("US/Central")),
+    end_date=None,
+    schedule=None, # '0 9 20 5 *',
+    dag_id="baby_name_etl",
+    dagrun_timeout=timedelta(minutes=5),
+    catchup=False,
+    description="baby name etl from SSA records",
+    max_active_runs=3,
+    tags=['demo'],
+    default_args=default_args
+)
+def etl_baby_names():
+
+    @task
+    def extract_baby_name_data():
+        
+        # Download Baby Name Data to Central Location
+        response = HttpHook(method='GET', http_conn_id='AIRFLOW_CONN_SSA').run(endpoint='oact/babynames/names.zip')
+
+        # Write Response Content to File System
+        with open("/home/zfreeze/airflow/dags/names.zip", "wb") as temp_file:
+            temp_file.write(response.content)
+
+    
+    # Bash to extract files from zip
+    # Bash to remove non-txt files
+    # Bash task remove files not matching the desired year
+
+    # Upload CSV -> tuples -> insert rows to Postgres using Python
+
+    # Bash to clean up all files in the directory
+    
+    # Define task dependencies
+    extract_baby_name_data()
+
+# Instantiate DAG
+etl_baby_names()
