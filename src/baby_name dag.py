bb68e434e5efb6e6ec4bb091dbc08209b4445a7a
zfreeze <zdfreeze11@gmail.com>
2025-02-27 20:24:31 -0600

    update to include base postgres connection, csv/glob to read files to upload

--- src/baby_name dag.py
+++ src/baby_name dag.py
@@ -1,11 +1,9 @@
 from datetime import datetime, timedelta
 import pendulum
+import logging
 import os
-from pathlib import Path
 from airflow.decorators import dag, task
-from airflow.providers.postgres.hooks.postgres import PostgresHook
 from airflow.operators.bash import BashOperator
-from airflow.providers.http.hooks.http import HttpHook
 
 
 # Define default arguments
@@ -36,6 +34,8 @@ def etl_baby_names():
 
     @task
     def extract_baby_name_data():
+        from airflow.providers.http.hooks.http import HttpHook
+
         # Download Baby Name Data to Central Location
         response = HttpHook(method='GET', http_conn_id='AIRFLOW_CONN_SSA').run(endpoint='oact/babynames/names.zip')
 
@@ -51,9 +51,33 @@ def etl_baby_names():
 
     @task
     def upload_baby_names():
-        # Upload CSV -> tuples -> insert rows to Postgres using Python
-        print(f"To be implemented!")
-        pass
+        import glob
+        import csv
+        from airflow.providers.postgres.hooks.postgres import PostgresHook
+
+        # Define CSV Directory & Table Name
+        TABLE_NAME: str = "baby_names.names"
+
+        # Define static column names (since CSV files have no headers)
+        COLUMN_NAMES: list[str] = ["year", "name", "gender", "count"]
+
+        # Connect to PostgreSQL
+        conn = PostgresHook(postgres_conn_id='AIRFLOW_CONN_POSTGRES_ANALYTICS')
+        csv_files = glob.glob(os.path.join(work_dir_str + '/names', "*.txt"))
+        print(f"{csv_files=}")
+
+        # Use glob to get all files:
+        for file in csv_files:
+            print(f"Processing {file=}...")
+
+            with open(file, "r") as f:
+                reader = csv.reader(f)
+                batch = []
+                for row in reader:
+                    batch.append(row)
+
+            conn.insert_rows(table=TABLE_NAME, rows=batch, targe_fields=COLUMN_NAMES, commit_every=0)
+            print(f"Finished processing {file}")
 
     clean_working_files = BashOperator(
         task_id='clean_working_files',
